50c3ee3fa861d31867d7722a0079da4a791c03c4
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-04 17:17:39 -0500

    Se inicializó el proyecto con el cli de vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Hi, this will be my portfolio"/>
   </div>
 </template>
+
+
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
