7b93eb66a44479bec8adc99f41a5299d1850baa4
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-20 20:13:23 -0500

    Se cambio el estilo del nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link> 
     <router-link to="/projects">Projects</router-link>
   </nav>
 
@@ -14,5 +14,16 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+}
+nav {
+  display: flex;
+  gap: 10px;
+  list-style: none;
+  justify-content: flex-end;
+  padding: 10px 50px 0 0;
+}
+nav > * {
+  text-decoration-line: none;
+  color: rgb(210, 202, 120);
 }
 </style>
