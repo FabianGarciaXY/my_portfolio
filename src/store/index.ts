af6b039d295aec1ec5d7e7864a449fdb5728097e
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-22 19:17:18 -0500

    Se agrego la información de los proyectos

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,40 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+
+    projects: {
+      project_1: {
+        name: 'Project 1',
+        repository: ''
+      },
+      project_2: {
+        name: 'project 2',
+        repository: ''
+      },
+      project_3: {
+        name: 'project 3',
+        repository: ''
+      },
+      project_4: {
+        name: 'project 4',
+        repository: ''
+      },
+      project_5: {
+        name: 'project 5',
+        repository: ''
+      },
+      project_6: {
+        name: 'project 6',
+        repository: ''
+      }
+    }
   },
   getters: {
+
+    getProjects(state) {
+      return state.projects
+    }
+
   },
   mutations: {
   },
