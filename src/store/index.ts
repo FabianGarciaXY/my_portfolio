3954e5ba441e14e47045b089f911d577d1017a9d
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-24 00:10:36 -0500

    Se agregaron datos de los repositorios

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,28 +5,35 @@ export default createStore({
 
     projects: {
       project_1: {
-        name: 'Project 1',
-        repository: ''
+        name: 'Restaurant Page with Vue Js',
+        repo: 'https://github.com/fabiangarciaxy/Practica_Semana5_Vue',
+        link : 'fabiangarciaxy.github.io/Practica_Semana5_Vue',
+        img: '../assets/image1.png'
       },
       project_2: {
-        name: 'project 2',
-        repository: ''
+        name: ' Etch a Sketch',
+        repository: '',
+        img: '../assets/image1.png'
       },
       project_3: {
         name: 'project 3',
-        repository: ''
+        repository: '',
+        img: ""
       },
       project_4: {
         name: 'project 4',
-        repository: ''
+        repository: '',
+        img: ""
       },
       project_5: {
         name: 'project 5',
-        repository: ''
+        repository: '',
+        img: ""
       },
       project_6: {
         name: 'project 6',
-        repository: ''
+        repository: '',
+        img: ""
       }
     }
   },
