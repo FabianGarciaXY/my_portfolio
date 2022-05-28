a84b355a6c42439bf6688cc66a3d9164693740fb
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-27 23:00:44 -0500

    Se agregaron indices a los proyectis

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,41 +5,40 @@ export default createStore({
 
     projects: {
       project_1: {
+        id_el: "a",
         name: 'Restaurant Page with Vue Js',
         link : 'https://fabiangarciaxy.github.io/Practica_Semana5_Vue/#/',
         repository: 'https://github.com/FabianGarciaXY/Practica_Semana5_Vue',
         img: 'image1'
       },
       project_2: {
+        id_el: "b",
         name: ' Etch a Sketch',
         link: 'https://fabiangarciaxy.github.io/Etch_a_Sketch/',
         repository: 'https://github.com/FabianGarciaXY/Etch_a_Sketch',
         img: 'image2'
       },
       project_3: {
+        id_el: "c",
         name: 'Hack The Ocean',
         link: '',
         repository: 'https://github.com/OmarVenturaP/HackTheOcean',
         img: "image3"
       },
       project_4: {
+        id_el: "d",
         name: 'Calculator',
         link: ' https://fabiangarciaxy.github.io/Calculator/',
         repository: 'https://github.com/FabianGarciaXY/Calculator',
         img: "image4"
       },
       project_5: {
-        name: 'project 5',
+        id_el: "e",
+        name: 'Telegram Bot',
         link: '',
         repository: '',
         img: "image1"
       },
-      project_6: {
-        name: 'project 6',
-        link: '',
-        repository: '',
-        img: "image1"
-      }
     }
   },
   getters: {
@@ -54,5 +53,5 @@ export default createStore({
   actions: {
   },
   modules: {
-  }
-})
+  },
+});
