a204ae206838cb4b787694879137d0e81d4f700b
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-27 23:01:02 -0500

    Se cambiarion los botones

--- src/components/ProjectsComponent.vue
+++ src/components/ProjectsComponent.vue
@@ -2,34 +2,26 @@
 
     <div class="body">
         <h1>This Is A Page About My Projects</h1>
-        <div>
-            I'm a self-tought software developer from Mexico and I am passionate about coding in languajes like JS and Python.
-        </div>
         <div id="cards-cointainer">
             <div class="cards-element" 
                 v-for="project in displayProjects()" 
-                :key="project.name">
+                :key="project.id_el"
+                >
 
                 <h3>
                     {{project.name}}
                 </h3>
-
                 
                 <div class="data-container">
 
-                    <div class="figure"
-                        @mouseover="test = true"
-                        @mouseleave="test = false"
-                        :class="{ active: test }"
-                        >
+                    <div class="figure">
 
                         <div class="links">
-                            
                             <div>
                                 <button class="button">
                                     <a :href="project.link" target="_blank">
                                         Preview
-                                    </a> 
+                                    </a>
                                 </button>
                             </div>
                             <div>
@@ -40,12 +32,9 @@
                                 </button>
                             </div>
                         </div>
-
                         <img class="image"
                             :src="require(`./../assets/${project.img}.png`)"
                             alt="test">
-
-
                     </div>
 
                     <div>
@@ -71,8 +60,7 @@ export default defineComponent({
     data() {
         return {
          title: 'My projects',
-         test: false
-        }
+    }
     },
     computed: {
         getData() {
@@ -89,7 +77,7 @@ export default defineComponent({
 
 <style scoped>
 h1{
-    padding:50px 0;
+    padding:50px 0 0 0;
 }
 h3 {
     padding: 10px 0 20px 0;
@@ -105,7 +93,7 @@ h3 {
     gap: 30px;
     padding: 50px;
     margin: 8%;
-    box-shadow: 1px 2px 15px 2px rgba(0,0,0,0.5);
+    box-shadow: 5px 10px 15px 2px rgba(0,0,0,0.5);
 }
 .cards-element{
     border-radius: 10px;
@@ -115,13 +103,14 @@ h3 {
 
     position: relative;
     overflow: hidden;
-    box-shadow: 0px 1px 1px rgba(0,0,0,0.3);
+    box-shadow: 0px 0.5px 1px rgba(0,0,0,0.3);
     transition: box-shadow 0.3s ease-out;
 }
 .cards-element:hover{
-    box-shadow: 1px 2px 10px rgba(0,0,0,0.5);
+    box-shadow: 1px 3px 10px rgba(0,0,0,0.5);
 }
-.figure{
+
+.figure {
     border-radius: 5px;
     width: 35%;
     height: auto;
@@ -130,11 +119,12 @@ h3 {
     z-index: 1;
     overflow: hidden;
 }
-.figure:hover {
+
+/* .figure:hover {
     cursor: pointer;
     filter: brightness(0.6);
     box-shadow: 1px 2px 10px rgb(255, 255, 255);
-}
+} */
 .image {
     position: relative;
     z-index: 1;
@@ -150,27 +140,29 @@ h3 {
     gap: 20px;
 }
 .links{
+    padding: 10px 0;
+    z-index: 3;
+    display: flex;
+    justify-content: center;
     height: 40%;
     margin-top: 0px;
     display: flex;
-    opacity:0;
-}
-.links:hover{
-    z-index: 2;
-    display: flex;
-    opacity:100;
-    justify-content: center;
     gap:  20px;
-    transition: 0.5s;
 }
 
 .button {
+    opacity: 0.4;
     cursor: pointer;
     padding: 6px;
     width: 100px;
     background-color: rgba(0, 0, 0, 0.263);
     color: white;
     border-radius: 3px;
+}
+
+.button:hover {
+    opacity:1;
+    transition: 0.5s;
 }
 
 a {
