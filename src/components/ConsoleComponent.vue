14d56133f042d02ccd40dcbd80f195488a3c3446
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-28 20:45:57 -0500

    Se corrigieron las animaciones de consola

--- src/components/ConsoleComponent.vue
+++ src/components/ConsoleComponent.vue
@@ -10,16 +10,22 @@
 
             <div id="code-space">
                 <div>
-                    &lt;> <br>
+                    <br>
 
                         <div class="line-1 anim-typewriter">
-                            class Fabian():                        
+                           <b>class</b> Fabian():                        
                         </div>
-                        <div class="line-1">
-                        &nbsp;&nbsp;    def __init__(self):
+                        <div class="line-2 anim-typewriter2">
+                            &nbsp;&nbsp;<i>pass</i>
                         </div>
 
-                    &lt;/>
+
+                        <br><br>
+                        <div class="line-3 anim-typewriter3">
+                            ;
+                        </div>
+
+                    
                 </div>
             </div>
         </div>
@@ -86,28 +92,59 @@ export default defineComponent({
 }
 
 /*============ Text animation =============*/
-.line-1{
-    font-weight: bold;
+.line-1, .line-2, .line-3{
+    padding: 3px 0 3px 30px;
+    font-weight: 400;
     font-family: 'Anonymous Pro', monospace;  
     font-size: 18px;
     position: relative;
     width: auto;
-    border-right: 6px solid rgba(255,255,255,.75);
+    border-right: 3px solid rgba(255,255,255,.75);
     text-align: left;
     white-space: nowrap;
     overflow: hidden;
+    text-shadow: #FC0 1px 0 9.5px;
 }
 /* Animation */
 .anim-typewriter{
-    animation: typewriter 2s steps(15) 2s 1 normal both,
-    blinkTextCursor 600ms steps(22) 6 normal;
+    animation: typewriter 1.8s steps(13) 2s 1 normal both,
+    blinkTextCursor 600ms steps(30) 7 normal both;
 }
 @keyframes typewriter{
     from{width: 0;}
-    to{width: 8em;}
+    to{width: 150px;}
 }
 @keyframes blinkTextCursor{
   from{border-right-color: rgba(225, 242, 252, 0.85);}
   to{border-right-color: transparent;}
+}
+
+.anim-typewriter2{
+    animation: typewriter2 0.5s steps(3) 4s 1 normal both ,
+    blinkTextCursor2 600ms steps(30) 4s 4 normal both;
+}
+@keyframes typewriter2 {
+    0%{width: 0em;}
+    1% {width:1.5em;}
+    100%{width: 3.5em;}
+}
+@keyframes blinkTextCursor2{
+    0%{border-right-color: transparent;}
+    99%{border-right-color: rgba(225, 242, 252, 0.85);}
+    100%{border-right-color: transparent;}
+}
+
+.anim-typewriter3{ 
+    animation: typewriter3 3s steps(1) 10s 1 normal both,
+    blinkTextCursor3 600ms steps(30) 6.5s infinite normal both;
+}
+@keyframes typewriter3 {
+    0%{width: 0;}
+    100%{width: 0;}
+}
+@keyframes blinkTextCursor3{
+    0%{border-right-color: transparent;}
+    99%{border-right-color: rgba(225, 242, 252, 0.85);}
+    100%{border-right-color: transparent;}
 }
 </style>
