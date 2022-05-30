d315c56414897046ab6f219434b5f3537870dadb
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-30 01:33:17 -0500

    Se eliminarion las rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,14 +6,6 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'home',
     component: HomeView
-  },
-  {
-    path: '/projects',
-    name: 'projects',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ProjectsView.vue')
   }
 ]
 
